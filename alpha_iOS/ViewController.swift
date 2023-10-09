0e59b09c4c857916824d6fde4fd6aced8932aa34
arxungo <danil.rastypain@gmail.com>
2023-10-09 18:57:28 +0300

    added project

---
+++ alpha_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alpha_iOS
+//
+//  Created by Даня on 02.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
