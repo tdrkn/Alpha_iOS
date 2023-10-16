a650172e1994d5b5b62b8f4d3cb09e7890a53551
arxungo <danil.rastypain@gmail.com>
2023-10-16 19:39:04 +0300

    Added beer api

--- alpha_iOS/ViewController.swift
+++ alpha_iOS/ViewController.swift
@@ -11,7 +11,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemCyan
+        let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
+        URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
+            guard
+                let data = data,
+                let response,
+                error == nil
+            else{
+                return
+            }
+//            print(data)
+            let str = String(data: data, encoding: .utf8)
+            print("Полученные данные: \(str ?? "")")
+        }).resume()
         // Do any additional setup after loading the view.
     }
 
